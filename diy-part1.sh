11f661a4cc94c57014c0837edf1a4526d7792cca
xfoxolx <107661287+xfoxolx@users.noreply.github.com>
2025-03-01 15:16:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >>feeds.conf.default
+echo "src-git istore https://github.com/linkease/istore" >> ./feeds.conf.default
